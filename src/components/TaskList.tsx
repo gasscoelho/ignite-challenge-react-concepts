4807cea05cbb64f5a97c4faf5d240c3fe68251fe
Gabriel Coelho <gaaucocoelho@gmail.com>
2021-06-19 17:08:06 -0300

    Initial commit

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import '../styles/tasklist.scss'
+import "../styles/tasklist.scss";
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from "react-icons/fi";
+
+const MESSAGE_TASK_NOT_FOUND =
+  "Não foi possivel completar a ação por que a task não foi encontrada.";
+
+const MESSAGE_TASK_TITLE_EMPTY =
+  "Por favor, informe um título para a sua task.";
 
 interface Task {
   id: number;
@@ -12,18 +18,65 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  function isEmptyTitle() {
+    return !newTaskTitle;
+  }
+
+  function isTaskNotFound(index: number) {
+    return index === -1;
+  }
+
+  function findTaskIndex(id: number) {
+    return tasks.findIndex((task) => task.id === id);
+  }
+
+  function clearTaskTitle() {
+    setNewTaskTitle("");
+  }
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (isEmptyTitle()) {
+      return alert(MESSAGE_TASK_TITLE_EMPTY);
+    }
+
+    const newTask: Task = {
+      id: Date.now(),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    setTasks([...tasks, newTask]);
+
+    clearTaskTitle();
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const index = findTaskIndex(id);
+
+    if (isTaskNotFound(index)) {
+      return alert(MESSAGE_TASK_NOT_FOUND);
+    }
+
+    tasks[index].isComplete = !tasks[index].isComplete;
+
+    setTasks([...tasks]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const index = findTaskIndex(id);
+
+    if (isTaskNotFound(index)) {
+      return alert(MESSAGE_TASK_NOT_FOUND);
+    }
+
+    tasks.splice(index, 1);
+
+    setTasks([...tasks]);
   }
 
   return (
@@ -32,25 +85,32 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
-          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+          <button
+            type="submit"
+            data-testid="add-task-button"
+            onClick={handleCreateNewTask}
+          >
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
 
       <main>
         <ul>
-          {tasks.map(task => (
+          {tasks.map((task) => (
             <li key={task.id}>
-              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+              <div
+                className={task.isComplete ? "completed" : ""}
+                data-testid="task"
+              >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -61,14 +121,17 @@ export function TaskList() {
                 <p>{task.title}</p>
               </div>
 
-              <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+              <button
+                type="button"
+                data-testid="remove-task-button"
+                onClick={() => handleRemoveTask(task.id)}
+              >
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
-  )
+  );
 }
